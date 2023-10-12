d929fd4f08fd65261e50e34c78baa9497ee9b7d2
mhend56 <mhend56@gmail.com>
2023-10-13 10:34:46 +1300

    Reactquery all products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import { Filters, PaginationContainer, ProductsContainer } from '../components';
 import { customFetch } from '../utils';
 const url = '/products';
+const allProductsQuery = (queryParams) => {
+  const { search, category, company, sort, price, shipping, page } =
+    queryParams;
+  return {
+    queryKey: [
+      'products',
+      search ?? '',
+      category ?? 'all',
+      company ?? 'all',
+      sort ?? 'a-z',
+      price ?? 100000,
+      shipping ?? false,
+      page ?? 1,
+    ],
+    queryFn: () => customFetch(url, { params: queryParams }),
+  };
+};
+
 export const loader =
   (queryClient) =>
   async ({ request }) => {
@@ -9,7 +27,9 @@ export const loader =
       ...new URL(request.url).searchParams.entries(),
     ]);
 
-    const response = await customFetch(url, { params });
+    const response = await queryClient.ensureQueryData(
+      allProductsQuery(params)
+    );
     const products = response.data.data;
     const meta = response.data.meta;
     return { products, meta, params };
