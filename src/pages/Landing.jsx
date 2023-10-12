acce68a904fb5e7c2b47feb5d166b4edce43afa9
mhend56 <mhend56@gmail.com>
2023-10-13 09:42:53 +1300

    ReactQuery Landing Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { FeaturedProducts, Hero } from '../components';
 import { customFetch } from '../utils';
 const url = 'products?featured=true';
+
+const featuredProductsQuery = {
+  queryKey: ['featuredProducts'],
+  queryFn: () => customFetch(url),
+};
+
 export const loader = (queryClient) => async () => {
-  const response = await customFetch(url);
+  const response = await queryClient.ensureQueryData(featuredProductsQuery);
+
   const products = response.data.data;
   return { products };
 };
