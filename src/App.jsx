886483b8ac631f3920b503cd0e96c40a579fa318
mhend56 <mhend56@gmail.com>
2023-10-13 09:02:22 +1300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages';
+
+import { ErrorElement } from './components';
+// Loaders
+import { loader as landingLoader } from './pages/Landing';
+import { loader as singleProductLoader } from './pages/SingleProduct';
+import { loader as productsLoader } from './pages/Products';
+import { loader as checkoutLoader } from './pages/Checkout';
+import { loader as ordersLoader } from './pages/Orders';
+
+// Actions
+import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
+import { action as checkoutAction } from './components/CheckoutForm';
+import { store } from './store';
+// Set up router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+        errorElement: <ErrorElement />,
+        loader: productsLoader,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      { path: 'about', element: <About /> },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+        loader: checkoutLoader(store),
+        action: checkoutAction(store),
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+        loader: ordersLoader(store),
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+    action: loginAction(store),
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+    action: registerAction,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
